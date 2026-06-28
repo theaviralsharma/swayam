4feaba11ed353c3d1a64ecc92b31ed391f9a253c
Aviral Sharma <beingaviralsharma@gmail.com>
2026-06-28 12:11:17 +0530

    fix

--- nou25_mg24/certificate/script.js
+++ nou25_mg24/certificate/script.js
@@ -3,22 +3,14 @@ const VALID_TOKEN =
 
 const PDF = "../pdf/255620112350_nou25_mg24.pdf";
 
-// Get token
+// Get token from URL
 const token = new URLSearchParams(window.location.search).get("q");
 
-// Invalid token
+// Redirect if token is invalid
 if (token !== VALID_TOKEN) {
     window.location.replace("https://swayam.gov.in");
     throw new Error("Invalid token");
 }
 
-// Detect iPhone / iPad Safari
-const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-// iPhone -> Open native PDF viewer
-if (isiOS) {
-    window.location.replace(PDF);
-} else {
-    // Desktop + Android
-    document.getElementById("pdfViewer").src = PDF;
-}
+// Load certificate
+document.getElementById("pdfViewer").src = PDF;
